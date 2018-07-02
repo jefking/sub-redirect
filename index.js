21f18a33e423948176649fac553870d1748090d7
root <root@DESKTOP-74N9G6D.localdomain>
2018-07-02 09:03:49 -0700

    formatting

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ http.createServer(function (req, res) {
     let sub = process.env.SUBDOMAIN || "www";
     let subdomain = sub + "." + req.headers.host;
 
-    res.writeHead(301, {"Location": subdomain});
+    res.writeHead(301, { "Location": subdomain });
     res.end();
 }).listen(process.env.PORT || 3000);
