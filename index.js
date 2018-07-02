57483dc419e94ae2e1520def5537b8653029ea76
root <root@DESKTOP-74N9G6D.localdomain>
2018-07-02 09:00:13 -0700

    redirect

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require("http");
 
 http.createServer(function (req, res) {
-    res.writeHead(301, {"Location": "http://jefking.me"});
+    let subdomain = "www" + req.headers.host;
+
+    res.writeHead(301, {"Location": subdomain});
     res.end();
 }).listen(process.env.PORT || 3000);
