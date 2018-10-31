1a1880dfca255b1dbb063298839f6d4c972381bd
Jef King <jefkin@microsoft.com>
2018-10-31 13:44:21 -0700

    redirect

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 var http = require("http");
 
 http.createServer(function (req, res) {
-    let sub = process.env.SUBDOMAIN || "www";
-    let protocol = process.env.PROTOCOL || "http";
-
-    let location = protocol + "://" + sub + "." + req.headers.host;
-
-    if (req.url)
-    {
-        location += req.url;
-    }
+    let location = process.env.LOCATION;
 
     res.writeHead(301, { "Location": location });
     res.end();
+    
 }).listen(process.env.PORT || 3000);
