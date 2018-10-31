7ec1edb2483f59bff39e9fbf6f29aecc24077726
Jef King <jefkin@microsoft.com>
2018-10-31 13:52:55 -0700

    less code

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var http = require("http");
 
 http.createServer(function (req, res) {
-    let location = process.env.LOCATION;
-
-    res.writeHead(301, { "Location": location });
+    res.writeHead(301, { "Location": process.env.LOCATION });
     res.end();
-    
 }).listen(process.env.PORT || 3000);
